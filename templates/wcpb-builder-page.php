95d84f283eb30817f83f6cc5e69fb8f8241720f9
DeaJae <DeaJae@users.noreply.github.com>
2014-08-08 14:57:16 +0100

    Update wcpb-builder-page.php
    added more options and instructions

--- templates/wcpb-builder-page.php
+++ templates/wcpb-builder-page.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * WooCommerce Product Builder Page Template
- * @author Philipp Bauer <philipp.bauer@vividdesign.de>
- * @version 0.9
+ * @author Philipp Bauer <philipp.bauer@vividdesign.de> | updated by Darren Jermy (littleportitservices.co.uk)
+ * @version 0.9.1
  */
 
 /* HANDLE POST/GET DATA */
@@ -16,6 +16,8 @@ do_action( 'wcpb_before_product_builder', $mix_request );
 	
 	<div id="wcpb-config-selection">
 		<?php do_action( 'wcpb_include_template', 'templates/wcpb-category-tablist.php' ); ?>
+	<!--	< ?php do_action( 'wcpb_include_template', 'templates/wcpb-category-tablist-woocats.php' ); ?> enable for categories directly from Woo -->
+		
 		<?php do_action( 'wcpb_include_template', 'templates/wcpb-category-options.php' ); ?>
 		<div class="clearfix"></div>
 	</div>
@@ -28,7 +30,7 @@ do_action( 'wcpb_before_product_builder', $mix_request );
 		<!--< ?php do_action( 'wcpb_include_template', 'templates/wcpb-review-textlist.php' ); ?> /not need in cart based-->
 		<!-- Price -->
 		<!--< ?php  do_action( 'wcpb_include_template', 'templates/wcpb-review-price.php' ); ?> //not needed if using cart method, kept for showing how it works -->
-		<?php do_action( 'wcpb_include_template', 'templates/wcpb-review-cart.php' ); ?>
+		<?php do_action( 'wcpb_include_template', 'templates/wcpb-review-cart.php' ); ?>  <!-- comment out to use woo's minicart in sidebar -->
 	</div>
 	
 	<div class="clearfix"></div>
