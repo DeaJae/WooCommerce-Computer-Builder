646c15f8e44896b8355c01538dc8158c28fdae90
DeaJae <DeaJae@users.noreply.github.com>
2015-07-09 16:25:52 +0100

    tidying up code for V1
    removing unused assets and code

--- templates/wcpb-builder-page.php
+++ templates/wcpb-builder-page.php
@@ -19,18 +19,6 @@ do_action( 'wcpb_before_product_builder', $mix_request );
 		<?php do_action( 'wcpb_include_template', 'templates/wcpb-category-options.php' ); ?>
 		<div class="clearfix"></div>
 	</div>
-	
-	<div id="wcpb-config-review">
-		<!-- Attribute Modification -->
-		<!--< ?php do_action( 'wcpb_include_template', 'templates/wcpb-review-attribute.php' ); ?> / Doesn't do anything currently--> 
-		<!-- Option Lists -->
-		<!--< ?php do_action( 'wcpb_include_template', 'templates/wcpb-review-thumblist.php' ); ?>	not needed -->	
-		<!--< ?php do_action( 'wcpb_include_template', 'templates/wcpb-review-textlist.php' ); ?> /not need in cart based-->
-		<!-- Price -->
-		<!--< ?php  do_action( 'wcpb_include_template', 'templates/wcpb-review-price.php' ); ?> //not needed if using cart method, kept for showing how it works -->
-		<!--< ?php do_action( 'wcpb_include_template', 'templates/wcpb-review-cart.php' ); ?> </!--disabled for sidebar cart-->
-	</div>
-	
-	<div class="clearfix"></div>
+		<div class="clearfix"></div>
 	
 </div>
